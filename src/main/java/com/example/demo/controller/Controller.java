c002c6eedf1ff918d8a6eec8e54c45cc2c08fdb8
LeoanrdoAvila18 <avilamiguel200318@gmail.com>
2024-10-15 19:34:56 -0500

    .

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Model;
+import com.example.demo.model.Person;
 import com.example.demo.persistence.Persistence;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 @RestController
 public class Controller {
     private Model model;
@@ -17,8 +28,9 @@ public class Controller {
 		model = new Model();
 	}
     @GetMapping("/")
-	public String run() {
-		return "Nombre: "+ this.selectName()+" "+this.selectLastName();
+	public Person run() throws UnknownHostException {
+		String hostName= InetAddress.getLocalHost().getHostName();
+		return new Person(LocalDateTime.now(),hostName,this.selectName(),this.selectLastName());
 	}
 	public String selectLastName(){
 		ArrayList<String> lastNamesList = persistence.readLastNamesFile();
@@ -27,5 +39,39 @@ public class Controller {
 	public String selectName(){
 		ArrayList<String> namesList = persistence.readNamesFile();
 		return namesList.get(model.generateRandomName(namesList));	
+	}
+	@DeleteMapping
+	public Person deletePerson(@RequestBody Person person){
+		ArrayList<String> namesList = persistence.readNamesFile();
+		ArrayList<String> lastNamesList = persistence.readLastNamesFile();
+		if(person.getName()!=null && person.getName()==null ){
+			namesList.remove(person.getName());
+		if(person.getName()==null && person.getName()!=null ){
+			lastNamesList.remove(person.getLastName());
+		}if(person.getName()!=null && person.getName()!=null ){
+			namesList.remove(person.getName());
+			lastNamesList.remove(person.getLastName());
+		}
+		persistence.writeNamesFile(namesList);
+		persistence.writeLastNamesFile(lastNamesList);
+	}
+		return person;
+	}
+	@PostMapping()
+	public Person addPerson(@RequestBody Person person){
+		ArrayList<String> namesList = persistence.readNamesFile();
+		ArrayList<String> lastNamesList = persistence.readLastNamesFile();
+		if(person.getName()!=null && person.getName()==null ){
+			namesList.add(person.getName());
+		if(person.getName()==null && person.getName()!=null ){
+			lastNamesList.add(person.getLastName());
+		}if(person.getName()!=null && person.getName()!=null ){
+			namesList.add(person.getName());
+			lastNamesList.add(person.getLastName());
+		}
+		persistence.writeNamesFile(namesList);
+		persistence.writeLastNamesFile(lastNamesList);
+	}
+		return person;
 	}
 }
--- src/main/java/com/example/demo/persistence/Persistence.java
+++ src/main/java/com/example/demo/persistence/Persistence.java
@@ -3,6 +3,9 @@ package com.example.demo.persistence;
 import java.io.*;
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class Persistence {
     	public ArrayList<String> readNamesFile() {
 		ArrayList<String> namesArray = new ArrayList<String>();
@@ -17,6 +20,30 @@ public class Persistence {
 			e.printStackTrace();
 		}
 		return namesArray;
+	}
+	@PostMapping("/name")
+	public void writeNamesFile(@RequestBody ArrayList<String> names) {
+		String nameRoute =System.getProperty("user.dir")+"/Libraries/Nombres.txt";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(nameRoute, false))){
+				for (String name : names) {
+				bw.write(name);
+				bw.newLine();
+				}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	@PostMapping("/lastName")
+	public void writeLastNamesFile(@RequestBody ArrayList<String> lastNames) {
+		String lastNamesRoute = System.getProperty("user.dir")+"/Libraries/Apellidos.txt";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(lastNamesRoute, false))){
+				for (String lastName : lastNames) {
+				bw.write(lastName);
+				bw.newLine();
+				}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	public ArrayList<String> readLastNamesFile() {
 		ArrayList<String> lastNames = new ArrayList<String>();
